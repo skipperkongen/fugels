df4420e9d77bd932b32438a205acd126fd3ae573
Pimin Konstantin Kefaloukos <skipperkongen@gmail.com>
2022-12-29 19:30:19 +0100

    fugles

--- modules/model.js
+++ modules/model.js
@@ -1,8 +1,5 @@
 let state = {
-    numFugels: 0,
-    numMashedFugels: 0,
-    numGenerators: 0,
-    numMashers: 0,
+    x: 0,
 }
 
 export { state }
--- modules/view.js
+++ modules/view.js
@@ -2,18 +2,6 @@ let elements = {
     // ASCII
     asciiDiv: document.getElementById('asciiDiv'),
     blinkendeKyllingPre: document.getElementById('blinkendeKyllingPre'),
-    // generator
-    generatorDiv: document.getElementById('generatorDiv'),
-    generatorIncrButton: document.getElementById('generatorIncrButton'),
-    generatorCounterSpan: document.getElementById('generatorCounterSpan'),
-    // masning
-    masningDiv: document.getElementById('masningDiv'),
-    masningIncrButton: document.getElementById('masningIncrButton'),
-    masningCounterSpan: document.getElementById('masningCounterSpan'),
-    // fugels
-    fugelsDiv: document.getElementById('fugelsDiv'),
-    fugelCounterSpan: document.getElementById('fugelCounterSpan'),
-    fugelMastSpan: document.getElementById('fugelMastSpan'),
 };
 
 export { elements }
